ff859e622983fbe969468dc62815c0409b0bdd15
Yada Ly <lyyada@gmail.com>
2022-03-07 11:58:34 +0100

    add errorPage

---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import "./ErrorPage.css";
+
+function ErrorPage() {
+  return (
+    <div className="errorPage">
+      <h1>404</h1>
+      <span>La page que vous demandez n'existe pas.</span>
+    </div>
+  );
+}
+
+export default ErrorPage;
